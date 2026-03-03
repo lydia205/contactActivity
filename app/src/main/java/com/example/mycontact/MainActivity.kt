b822298c29b9adc8a871fbc7487bf2361f033ba5
lydia205 <lydiaomombo35@gmail.com>
2026-03-03 11:54:21 +0200

    complited project

--- app/src/main/java/com/example/mycontact/MainActivity.kt
+++ app/src/main/java/com/example/mycontact/MainActivity.kt
@@ -5,18 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val FristName ="GRACE"
+        val firstName ="GRACE"
         val lastName ="Quenn"
-        val Address ="cape town"
+        val address ="cape town"
         val isMale="false"
         val dayOfBirth="06"
         val monthOfBirth="june"
-        val yearOtbirth="2007"
+        val yearOfBirth="2007"
+        Log.d("Hey I am ", firstName + " " + lastName + "and I live in " + address +
+                " and I was born on "+ dayOfBirth + " " + monthOfBirth + " " + yearOfBirth)
 
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
