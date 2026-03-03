7336cee89fbf666218ca41c9ef8c5a04c1214e40
lydia205 <lydiaomombo35@gmail.com>
2026-03-03 10:52:28 +0200

    added variables

---
+++ app/src/main/java/com/example/mycontact/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mycontact
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val FristName ="GRACE"
+        val lastName ="Quenn"
+        val Address ="cape town"
+        val isMale="false"
+        val dayOfBirth="06"
+        val monthOfBirth="june"
+        val yearOtbirth="2007"
+
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
