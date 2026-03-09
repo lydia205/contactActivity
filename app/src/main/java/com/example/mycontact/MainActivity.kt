a1107e63c18ad12c70e10c689a1b1bf23503ee8d
lydia205 <lydiaomombo35@gmail.com>
2026-03-09 16:23:50 +0200

    ice task 3

--- app/src/main/java/com/example/mycontact/MainActivity.kt
+++ app/src/main/java/com/example/mycontact/MainActivity.kt
@@ -21,6 +21,18 @@ class MainActivity : AppCompatActivity() {
         Log.d("Hey I am ", firstName + " " + lastName + "and I live in " + address +
                 " and I was born on "+ dayOfBirth + " " + monthOfBirth + " " + yearOfBirth)
 
+        val age = 19
+        val yearsTo100 = 100 - age
+        val ageInMonth = age *12
+        val ageInDays=age*365
+        val remainder =age%5
+
+        println("Age: $age")
+        println("You will be 100 in $yearsTo100 years")
+        println("Your age in months: $ageInMonth")
+        println("your age in days: $ageInDays")
+        println("your remaindre :$remainder")
+        
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
